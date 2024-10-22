1c9d3bc532250ba26cf05b4a7eae7e54a3fd24d1
bun137 <gurram13775@gmail.com>
2024-10-22 09:51:26 +0530

    adds wallpaper

--- .config/hypr/scripts/sleep.sh
+++
@@ -1,5 +0,0 @@
-swayidle -w timeout 60 'swaylock -f -c 000000' \
-            timeout 120 'hyprctl dispatch dpms off' \
-            resume 'hyprctl dispatch dpms on' \
-            timeout 120 'systemctl suspend' \
-            before-sleep 'swaylock -f -c 000000' &
